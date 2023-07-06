75a2b9893f6fb4ff4994d6c967436ff55c6d560c
dafnemus <dafnemus@gmail.com>
2023-07-06 15:07:58 -0300

    added EditiContact route into browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact from "./routes/edit";
 import ErrorPage from "./error-page";
 import "./index.css";
 
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
