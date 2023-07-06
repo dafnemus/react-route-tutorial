8c262e0cf8b24662db7a6a1d5c6ea2e725849b3e
dafnemus <dafnemus@gmail.com>
2023-07-06 13:46:37 -0300

    added ErrorPage component and it added into router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/routes";
+import ErrorPage from "./error-page";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
